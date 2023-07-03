e6735dfbafb65bf72b6da958d53320f9e1192240
gumelarfajar <138423225+gumelarfajar@users.noreply.github.com>
2023-07-03 12:47:01 +0700

    Delete application directory

--- application/views/includes/footer.php
+++
@@ -1,7 +0,0 @@
- <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 1.0.0
-    </div>
-    <strong>Copyright &copy; 2022 <a href="http://www.rajaputramedia.com" target="_blank">KASIR</a>.</strong> All rights
-    reserved.
-  </footer>
